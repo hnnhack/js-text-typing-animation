cc3e16113ad955e671c44ad624a71378d7d5aa1a
niyas <hojaniyazhan@yahoo.com>
2023-02-22 10:58:27 +0100

    chat typed added

---
+++ index.js
@@ -0,0 +1,13 @@
+const typed = new Typed('#type-space', {
+  strings: ['Hi there!', 'Welcome!', 'I am chatbot, Billy', 'How can I help you?'],
+  typeSpeed: 35,
+  startDelay: 500,
+  backDelay: 1000,
+  backSpeed: 35,
+  loop: true,
+  loopCount: 2,
+  // fadeOut: true,
+  // showCursor: false
+  // cursorChar: '...'
+
+});
